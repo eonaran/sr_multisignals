d2d2e8ad8de446f9de54717d54ed89fe90d34b53
eonaran <30991014+eonaran@users.noreply.github.com>
2017-10-13 00:25:10 -0400

    Add files via upload

--- min_distance_mm.py
+++ min_distance_mm.py
@@ -7,20 +7,39 @@ Created on Wed Oct 11 13:21:22 2017
 import numpy as np
 import matplotlib.pyplot as plt
 
+def build_support(distance, n_spikes, jitter_ratio, jitter_factor):
+    
+    return(0);
+
+
+def feasibility_test_mm(tspikes, fc,pattern,K):
+    
+    return(0)
+    
 def main():
     repetitions = 2;
     jitter_factor = 100;
     jitter_ratio=0.75;
-    fc_vector = 40;#[30 40 50];
+    fc_vector = np.array([40]); #[30 40 50];
     n_spikes=25;
     K=4;
     distance_vector = [] ;
     successes = [];
     
-    for ind_fc in range(len(fc_vector)):
+    for ind_fc in range(fc_vector.size ):
         fc = fc_vector[ind_fc];
-        distance_vector.append([0.64]);#( 0.6:0.01:1 );
-        successes.append(np.zeros((len(distance_vector[ind_fc]),1)));
+        distance_vector.append(np.array([0.64]));#( 0.6:0.01:1 );
+        successes.append(np.zeros((distance_vector[ind_fc].size,1)));
+        
+        for ind_distance in range(distance_vector[ind_fc].size):
+            distance = distance_vector[ind_fc][ind_distance]/fc;
+            
+            for ind_rep in range(repetitions):
+                tspikes = build_support(distance, n_spikes, jitter_ratio, jitter_factor);
+                aux_pattern = np.random.randn(n_spikes,K);
+                pattern = aux_pattern/np.matlib.repmat(np.sqrt(np.sum(np.conj(aux_pattern)*aux_pattern,2 )),1,K);
+                successes[ind_fc][ind_distance] = successes[ind_fc][ind_distance]+feasibility_test_mm(tspikes, fc,pattern,K)    
+        
         
 if __name__ == "__main__" :
     main()
