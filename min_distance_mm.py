0aaac09e8d00c4fc09c17cbaef2f929509b25351
eonaran <30991014+eonaran@users.noreply.github.com>
2017-11-01 00:05:03 -0400

    Add files via upload

--- min_distance_mm.py
+++ min_distance_mm.py
@@ -7,20 +7,37 @@ Created on Wed Oct 11 13:21:22 2017
 import numpy as np
 import matplotlib.pyplot as plt
 
+def dirichlet(f,t):
+    if t== 0:
+        return(1)
+    else:
+        return(np.sin((2*f+1)*np.pi*t)/ ((2*f+1)*np.sin(np.pi*t) ) )
+
+def dirichlet_1(f,t):
+    if t==0:
+        return(0)
+    else:
+        return(np.pi/np.sin(np.pi*t) * (np.cos((2*f+1)*np.pi*t )-dirichlet(f,t)*np.cos(np.pi*t) ) )
+        
+
 def build_support(distance, n_spikes, jitter_ratio, jitter_factor):
-    jitter = distance/jitter_factor;
-    
-    tspikes_aux = np.arange(distance/2, (n_spikes+2)*distance, distance );
+    jitter = distance/jitter_factor;    
+    tspikes_aux = np.linspace(distance/2, (n_spikes+3/2)*distance, num=n_spikes+2 );
     #offsets = np.cumsum(jitter*np.random.rand((n_spikes,1))* (np.random.rand((n_spikes,1))<jitter_ratio) );
     offsets =0;
     tspikes = tspikes_aux[0:n_spikes] + offsets;
-    return(tspikes); 
+    return(tspikes, (n_spikes+2)*distance); 
     
 
-def feasibility_test_mm(tspikes, fc,pattern,K):
+def kernel_fit(tspikes, distance, fc, pattern, K):
     nspikes = tspikes.size; 
-        
-    
+    t = np.linspace(distance/2, (nspikes+3/2)*distance, 10*nspikes );
+    t = np.outer(t, np.ones((1, nspikes))) - np.outer(np.ones((t.size, 1)), tspikes);
+    dirichlet_vec = np.vectorize(dirichlet, otypes=[np.float])
+    M = dirichlet_vec(fc, t);
+    dirichlet_1_vec = np.vectorize(dirichlet_1, otypes=[np.float])
+    M = np.concatenate((M , dirichlet_1_vec(fc,t)), axis=1 );
+    print(np.shape(M))
     return(0)
     
 def main():
@@ -42,11 +59,11 @@ def main():
             distance = distance_vector[ind_fc][ind_distance]/fc;
             
             for ind_rep in range(repetitions):
-                tspikes = build_support(distance, n_spikes, jitter_ratio, jitter_factor);
+                tspikes, interval = build_support(distance, n_spikes, jitter_ratio, jitter_factor);
                 aux_pattern = np.random.randn(n_spikes,K)+1j*np.random.randn(n_spikes,K);
                 pattern = aux_pattern/np.matlib.repmat(np.sqrt(np.sum(np.conj(aux_pattern)*aux_pattern,1 )),1,K);
-                successes[ind_fc][ind_distance] = successes[ind_fc][ind_distance]+feasibility_test_mm(tspikes, fc,pattern,K)    
-        
-        
+                successes[ind_fc][ind_distance] = successes[ind_fc][ind_distance]+kernel_fit(tspikes, distance, fc, pattern,K)    
+    
+#    print(tspikes)        
 if __name__ == "__main__" :
     main()
