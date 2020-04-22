3d77c2e656b22fcfe53dbc430e6fa4de5bc69f66
Dan Turkel <daturkel@gmail.com>
2020-04-22 15:33:54 -0400

    experimentation on kernel matrices

---
+++ dt_and_ct_project/test_data.py
@@ -0,0 +1,213 @@
+import numpy as np
+import random as rn
+
+
+def _complex_gaussian():
+    x, y = np.random.normal(loc=0.0, scale=1.0, size=2)
+    return x + y * 1j
+
+
+def uniform_sign_pattern_1d_real(size):
+    return np.array([rn.choice([-1.0, 1.0]) for _ in range(size)])
+
+
+def uniform_sign_pattern_1d(size):
+    ret = []
+    for _ in range(size):
+        z = _complex_gaussian()
+        ret.append(z / np.linalg.norm(z))
+    return np.array(ret)
+
+
+def uniform_sign_pattern_multidim(size1, size2, orthogonal=True, sparse = True):
+    if orthogonal:
+        if sparse:
+            assert size2 >= size1
+            V = np.identity(size2)
+            return V[:size1, :]
+        else:
+            # Generate orthogonal columns by taking the QR decomposition of a
+            # random GUE matrix
+            ret = np.zeros((size1, size1))
+            iid_normals = (
+                np.random.normal(loc=0.0, scale=1.0, size=(size1, size1)) +
+                np.random.normal(loc=0.0, scale=1.0, size=(size1, size1)) * 1j) / (
+                    np.sqrt(2.0))
+            gue = (iid_normals + iid_normals.T) / np.sqrt(2.0)
+            q = np.linalg.qr(gue)[0]
+            return q[:, :size2]
+    else:
+        ret = np.zeros((size1, size2)).astype(np.complex128)
+        for i in range(size1):
+            for j in range(size2):
+                ret[i, j] = _complex_gaussian()
+        norms = np.linalg.norm(ret, axis=1)
+        return (ret.T / norms).T
+
+
+class CircleInterval(object):
+    """Represents an interval on the circle thought of as [0, 1] interval."""
+
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.wraps_around = end < start
+        self.is_entire = start == 0.0 and end == 1.0
+
+    def contains(self, x):
+        if not self.wraps_around:
+            return self.start <= x <= self.end
+        else:
+            return x <= self.end or x >= self.start
+
+    def length(self):
+        if not self.wraps_around:
+            return self.end - self.start
+        else:
+            return 1.0 + self.end - self.start
+
+    def random(self):
+        """Samples a uniformly random point from the interval."""
+        if not self.wraps_around:
+            return self.start + np.random.random() * self.length()
+        else:
+            out = self.start + np.random.random() * self.length()
+            if out >= 1.0:
+                return out - 1.0
+            else:
+                return out
+
+    def split_with_separation(self, x, delta):
+        assert self.contains(x)
+
+        if delta == 0.0:
+            return [self]
+
+        lo = x - delta
+        hi = x + delta
+        if hi >= 1.0:
+            hi = hi - 1.0
+        if lo <= 0.0:
+            lo = lo + 1.0
+
+        if self.is_entire:
+            return [CircleInterval(hi, lo)]
+        else:
+            resulting_intervals = []
+
+            if self.contains(lo):
+                resulting_intervals.append(CircleInterval(self.start, lo))
+            if self.contains(hi):
+                resulting_intervals.append(CircleInterval(hi, self.end))
+
+            return resulting_intervals
+
+    def __repr__(self):
+        return 'CircleInterval(%.2f, %.2f)' % (self.start, self.end)
+
+
+def lrange(r1, inc, r2):
+    n = ((r2-r1)+2*np.spacing(r2-r1))//inc
+    return np.linspace(r1, r1 + inc*n, n+1)
+
+
+def jittered_supports(
+        size,
+        min_separation,
+        jitter_ratio=0.5,
+        jitter_factor=10):
+    jitter = min_separation / jitter_factor
+    base = lrange(
+        min_separation * 0.5,
+        min_separation,
+        (size + 1.0) * min_separation)
+    offsets = np.cumsum(
+        jitter *
+        np.random.random(size=size) *
+        (np.random.random(size=size) < jitter_ratio))
+    candidate = base[:size] + offsets
+
+    candidate = np.array(sorted([
+        (c if c <= 1.0 else c - 1.0) for c in candidate]))
+
+    if size > 1:
+        candidate_min_separation = min(
+            np.min(candidate[1:] - candidate[:-1]),
+            candidate[0] - candidate[-1] + 1.0) + 1e-8
+    else:
+        candidate_min_separation = float('inf')
+
+    # If the jitter has ruined the minimum separation property, return the base
+    # instead.
+    if candidate_min_separation >= min_separation:
+        return candidate[:size]
+    else:
+        print('Warning: unrandomized supports')
+        return base[:size]
+
+
+def uniform_supports(size, min_separation=None, max_iters=1000):
+    if min_separation is None:
+        min_separation = 0.0
+
+    assert size * min_separation <= 1.0
+
+    for _ in range(max_iters):
+        pts = []
+        intervals = [CircleInterval(0.0, 1.0)]
+        for _ in range(size):
+            if not intervals:
+                pts = []
+                break
+
+            # Choose a random interval, weighted by length:
+            lengths = np.array([interval.length() for interval in intervals])
+            probs = lengths / np.sum(lengths)
+            interval_ix = np.random.choice(len(intervals), 1, p=probs)[0]
+            interval = intervals[interval_ix]
+
+            # Choose a random point in the chosen interval
+            x = interval.random()
+
+            # Add to list of support points
+            pts.append(x)
+
+            # Adjust intervals list
+            resulting_intervals = interval.split_with_separation(
+                x, min_separation)
+            intervals.pop(interval_ix)
+            intervals.extend(resulting_intervals)
+        else:
+            break
+
+    if not pts:
+        return None
+    else:
+        return np.array(sorted(pts))
+
+
+def exact_supports(size, separation, max_iters=1000):
+    if separation is None:
+        separation = 0.0
+
+    pts = []
+    if not isinstance(separation, list):
+        assert (size-1) * separation <= 1.0
+        x = np.random.uniform(0, 1-(size-1)*separation )
+        pts.append(x);
+        for _ in range(size-1):
+            x = x + separation;
+            pts.append(x)
+    else:
+        assert sum(separation) <= 1.0
+        x = np.random.uniform(0, 1- sum(separation))
+        pts.append(x)
+        size = len(separation)+1
+        for k in range(size-1):
+            x = x + separation[k];
+            pts.append(x)
+
+    if not pts:
+        return None
+    else:
+        return np.array(sorted(pts))
---
+++ dt_and_ct_project/trig_poly.py
@@ -0,0 +1,156 @@
+import operator
+import numbers
+import numpy as np
+from functools import reduce
+
+
+class TrigPoly(object):
+
+    def __init__(self, freqs, coeffs):
+        assert len(freqs) == len(coeffs)
+        self.freqs_hash = hash(tuple(freqs))
+        self.freqs = np.array(freqs)
+        self.coeffs = np.array(coeffs)
+        self.coeff_dict = {f: c for f, c in zip(freqs, coeffs)}
+
+    @classmethod
+    def dirichlet(cls, f):
+        freqs = range(-f, f + 1)
+        coeffs = [1.0 / (2.0 * f + 1.0) for _ in range(len(freqs))]
+        return cls(freqs, coeffs)
+
+    @classmethod
+    def multi_dirichlet(cls, f, gammas):
+        return reduce(
+            operator.mul,
+            [cls.dirichlet(int(f * gamma)) for gamma in gammas],
+            cls.one())
+
+    @classmethod
+    def zero(cls):
+        return cls([0], [0.0])
+
+    @classmethod
+    def one(cls):
+        return cls([0], [1.0])
+
+    def eval(self, t):
+        if isinstance(t, numbers.Number):
+            t = np.array([t])
+        repeated_ts = np.repeat(
+            t.reshape([1] + list(t.shape)), len(self.freqs), axis=0)
+        reshaped_freqs = (
+            self.freqs.reshape([len(self.freqs)] + [1] * len(t.shape)))
+        reshaped_coeffs = (
+            self.coeffs.reshape([len(self.coeffs)] + [1] * len(t.shape)))
+        return np.sum(
+            reshaped_coeffs *
+            np.exp(2.0 * np.pi * 1j * repeated_ts * reshaped_freqs),
+            axis=0)
+
+    def conjugate(self):
+        return TrigPoly([-f for f in self.freqs], np.conj(self.coeffs))
+
+    def real(self):
+        return (self + self.conjugate()) * 0.5
+
+    def imag(self):
+        return (self + self.conjugate() * (-1.0)) * (-0.5 * 1j)
+
+    def shift(self, t):
+        """Returns the trig poly time-shifted by +t."""
+        new_coeffs = self.coeffs * np.exp(2.0 * np.pi * 1j * self.freqs * t)
+        return TrigPoly(self.freqs, new_coeffs)
+
+    def sum_shifts(self, shifts, coeffs):
+        return TrigPoly(
+            self.freqs,
+            sum(self.coeffs * np.exp(2.0 * np.pi * 1j * self.freqs * t) * c
+                for c, t in zip(coeffs, shifts)))
+
+    def squared_norm(self):
+        return np.sum(np.absolute(self.coeffs) ** 2)
+
+    def derivative(self):
+        return TrigPoly(
+            self.freqs, self.coeffs * 2.0 * np.pi * 1j * self.freqs)
+
+    def inners_of_shifts(self, ts):
+        n = ts.shape[0]
+
+        # deltas[i, j] = t_i - t_j
+        deltas = (
+            np.outer(ts, np.ones(n)) - np.outer(np.ones(n), ts)
+            ).reshape((n, n, 1)).repeat(len(self.freqs), axis=2)
+
+        return np.einsum(
+            'ijk,k->ij',
+            np.exp(2.0 * np.pi * 1j * deltas * self.freqs),
+            np.absolute(self.coeffs) ** 2)
+
+    def inners_of_shifts_and_derivative_shifts(self, ts):
+        n = ts.shape[0]
+
+        # deltas[i, j] = t_i - t_j
+        deltas = (
+            np.outer(ts, np.ones(n)) - np.outer(np.ones(n), ts)
+            ).reshape((n, n, 1)).repeat(len(self.freqs), axis=2)
+
+        return np.einsum(
+            'ijk,k,k->ij',
+            np.exp(2.0 * np.pi * 1j * deltas * self.freqs),
+            2.0 * np.pi * 1j * self.freqs,
+            np.absolute(self.coeffs) ** 2)
+
+    def __call__(self, t):
+        return self.eval(t)
+
+    def __add__(self, other):
+        if self.freqs_hash == other.freqs_hash:
+            return TrigPoly(self.freqs, self.coeffs + other.coeffs)
+
+        all_freqs = sorted(set(self.freqs) | set(other.freqs))
+        all_coeffs = [
+            self.coeff_dict.get(f, 0.0) + other.coeff_dict.get(f, 0.0)
+            for f in all_freqs]
+        return TrigPoly(all_freqs, all_coeffs)
+
+    def __mul__(self, other):
+        if isinstance(other, numbers.Number):
+            new_coeffs = [c * other for c in self.coeffs]
+            return TrigPoly(self.freqs, new_coeffs)
+        elif isinstance(other, TrigPoly):
+            min_freq = min(min(self.freqs), min(other.freqs))
+            max_freq = max(max(self.freqs), max(other.freqs))
+            all_freqs = range(min_freq, max_freq + 1)
+
+            all_self_coeffs = np.zeros(len(all_freqs), dtype=np.complex)
+            for f, c in zip(self.freqs, self.coeffs):
+                all_self_coeffs[f - min_freq] = c
+
+            all_other_coeffs = np.zeros(len(all_freqs), dtype=np.complex)
+            for f, c in zip(other.freqs, other.coeffs):
+                all_other_coeffs[f - min_freq] = c
+
+            new_freqs = range(2 * min_freq, 2 * max_freq + 1)
+            new_coeffs = np.convolve(all_self_coeffs, all_other_coeffs)
+
+            return TrigPoly(new_freqs, new_coeffs)
+        else:
+            assert False
+
+
+class MultiTrigPoly(object):
+
+    def __init__(self, polys):
+        assert all(isinstance(p, TrigPoly) for p in polys)
+        self.polys = polys
+
+    def derivative(self):
+        return MultiTrigPoly([p.derivative() for p in self.polys])
+
+    def eval(self, t):
+        return np.stack([p(t) for p in self.polys], axis=0)
+
+    def __call__(self, t):
+        return self.eval(t)
